4da1fa9b76b814d8eecc1dbc790380b75ada4aa3
Luvjeet Singh <65855002+Luvjeet@users.noreply.github.com>
2021-11-28 20:53:47 +0530

    isLoading dispatched

--- src/actions/detailAction.js
+++ src/actions/detailAction.js
@@ -2,8 +2,13 @@ import axios from "axios";
 import { gameDetailsUrl, gameScreenShotsUrl } from "../api";
 
 export const loadDetail = (id) => async (dispatch) => {
+    dispatch({
+        type: "LOADING_DETAIL",
+    });
+
     const detailData = await axios.get(gameDetailsUrl(id));
     const screenShotData = await axios.get(gameScreenShotsUrl(id));
+
     dispatch({
         type: "GET_DETAIL",
         payload: {
